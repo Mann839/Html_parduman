53c7e4daf7cff3a3c4713d0924f3382d2724c5c7
Mann839 <pradumankarol108@gmail.com>
2025-06-09 10:31:01 +0530

    15th commit

---
+++ javascript day-15/index.js
@@ -0,0 +1,8 @@
+// console.log(average([1, 2, 3])); 
+// const average = require("./module");
+const module = require("./module");
+console.log(module.name)
+// console.log(module.average([3,4])); 
+console.log('this is index.js');
+
+
