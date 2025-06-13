a1126f7288d144e5dfe5309afe13a81477603ff6
Mann839 <pradumankarol108@gmail.com>
2025-06-13 17:10:38 +0530

    17th commit

--- express day -17/app.js
+++ express day -17/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 80;
+
+//EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static'));//for serving static files
+
+//PUG SPECIFIC STUFF
+app.set('view engine', 'pug');//set the   template engine as pug
+app.set('views',path.join(__dirname, 'views'));//set the views directory
+
+//ENDPOINTS
+app.get('/',()=>{
+    res.status(200).render('index', { title: 'Hey', message: 'Hello there this is my first express app!' });
+})
+//our pug demo endpoint
+app.get('/demo', (req, res) => {
+    res.status(200).render('demo', { title: 'Hey', message: 'Hello there this is a demo!' });
+});
 
 app.get('/',(req,res)=>{
     res.send('This is the home page');
