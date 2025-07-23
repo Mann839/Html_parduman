92e9c9d0f8554bad10f21fc710ff644fccf85512
Mann839 <pradumankarol108@gmail.com>
2025-07-23 20:39:50 +0530

    18th commit

--- Dance website -18/app.js
+++ Dance website -18/app.js
@@ -4,19 +4,20 @@ const app = express();
 const port = 8000;
 
 //EXPRESS SPECIFIC STUFF
-app.use('/static', express.static('static'));//for serving static files
-app.use(express.urlencoded());
+app.use('/static',express.static('static')); // For serving static files
+app.use(express.urlencoded()); // For parsing form data
 
-//PUG SPECIFIC STUFF
-app.set('view engine', 'pug');//set the   template engine as pug
-app.set('views',path.join(__dirname, 'views'));//set the views directory
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug'); // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')); // Set the views directory
 
-//ENDPOINTS
-app.get('/',(req,res)=>{   
+// ENDPOINTS
+app.get('/', (req, res) => {
     const params = {};
-    res.status(200).render('index.pug', params);//render the index.pug file with params
-})
+    res.status(200).render('index.pug', params);
+});
+
 // START THE SERVER
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-}); 
+});
